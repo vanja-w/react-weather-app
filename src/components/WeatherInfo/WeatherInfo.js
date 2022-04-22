2062df169f629069089c872f7dd93cb5de4e9406
vanja-w <vanja.wallace@gmail.com>
2022-04-22 10:05:25 +0200

    Adds styling to displayed weather data

--- src/components/WeatherInfo/WeatherInfo.js
+++ src/components/WeatherInfo/WeatherInfo.js
@@ -10,31 +10,39 @@ export default function WeatherInfo(props) {
       {/*CURRENT DATE STARTS*/}
       <CurrentDate date={props.data.date} />
       {/*CURRENT DATE ENDS*/}
-      <div className="row ">
-        <div className="col-6">
-          <div className="col">
-            <WeatherIcon id={props.data.iconId} />
-          </div>
-          <div className="col">{props.data.description}</div>
+      <div className="row d-flex flex-column mb-3">
+        <div className="col d-flex justify-content-center">
+          <WeatherIcon id={props.data.iconId} />
         </div>
-        <div className="col-6">{Math.round(props.data.temperature)}°C</div>
+        <div
+          className="col d-flex justify-content-center"
+          id="weather-description"
+        >
+          {props.data.description}
+        </div>
+        <div
+          className="col d-flex justify-content-center"
+          id="current-temp-celsius"
+        >
+          {Math.round(props.data.temperature)}°C
+        </div>
       </div>
-      <div className="row">
-        <div className="col">
+      <div className="row d-flex">
+        <div className="col d-flex justify-content-center text-center">
           Wind <br />
           {Math.round(props.data.wind)} km/h
         </div>
-        <div className="col">
+        <div className="col d-flex justify-content-center text-center">
           Humidity
           <br /> {props.data.humidity}%
         </div>
-        <div className="col">
-          <div className="col">
+        <div className="col d-flex">
+          <div className="col d-flex justify-content-center text-center">
             Min
             <br />
             {Math.round(props.data.tempMin)}°
           </div>
-          <div className="col">
+          <div className="col d-flex justify-content-center text-center">
             Max
             <br />
             {Math.round(props.data.tempMax)}°
